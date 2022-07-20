6cf72ea1255c029c35d442cf4001610e0aa92c19
saskiabt <sskbinder@gmail.com>
2022-07-20 15:09:23 -0400

    cleaning up sticky header

--- src/sticky-header.js
+++ src/sticky-header.js
@@ -1,11 +1,10 @@
 // When the user scrolls the page, execute myFunction
 
 const stickyHeader = () => {
-    window.addEventListener('scroll', toggleSticky); 
+    window.addEventListener('scroll', toggleSticky);
     const header = document.getElementById('header');
     const sticky = header.offsetTop;
 
-    
     function toggleSticky() {
         if (window.pageYOffset > sticky) {
             header.classList.add('sticky');
