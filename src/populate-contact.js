75b3dae92f29b4b376e08b97e791b9986481d060
saskiabt <sskbinder@gmail.com>
2022-07-21 09:55:47 -0400

    link to source code in contact page

--- src/populate-contact.js
+++ src/populate-contact.js
@@ -7,17 +7,26 @@ const loadContact = () => {
         const main = createDivs(content, 'contact-content', '', 'content-class');
         createDivs(main, 'content-title', 'CONTACT US!');
         const info = createDivs(main, 'info-wrapper', '');
-        const address = createDivs(info, 'address-box', '', 'info-box');
-        const clickMe = createElem('p', address, '140 St Nicholas Ave, Brooklyn, New York 11237', 'contact-info');
-        clickMe.addEventListener('click', () => {
-            window.open('https://g.page/gordoscantina?share', 'blank');
-        });
         const number = createDivs(info, 'number-box', '', 'info-box');
         createElem('p', number, '(917)-947-9208', 'contact-info');
         const email = createDivs(info, 'email-box', '', 'info-box');
         createElem('p', email, 'info@gordoscantina.com', 'contact-info');
+        const tagWrapper = createDivs(main, 'tag-wrapper', '');
+        createElem('p', tagWrapper, 'Built by Saskia Binder', 'tag-info');
+        const sourceCode = createElem('p', tagWrapper, 'Source Code', 'tag-info');
+        sourceCode.id = 'source-code';
+        sourceCode.addEventListener('click', () => {
+            window.open('https://github.com/saskiabt/restaurant-website', 'blank');
+        });
+        const address = createDivs(info, 'address-box', '', 'info-box');
+        const clickMe = createElem('p', address, `140 St Nicholas Ave \ 
+                                                  Brooklyn, New York 11237`, 'contact-info')
+        clickMe.addEventListener('click', () => {
+            window.open('https://g.page/gordoscantina?share', 'blank');
+        });
     }
 };
+
 
 const hideContact = () => {
     if (document.querySelector('#contact-content')) document.querySelector('#contact-content').remove();
